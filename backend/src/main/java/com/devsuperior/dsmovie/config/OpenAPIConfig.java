08638c00f82c551557378bcad8648e95e205ac98
Leonardo <leomeireles66@gmail.com>
2025-04-07 10:12:05 -0300

    Cofigurations with Spring Security

--- backend/src/main/java/com/devsuperior/dsmovie/config/OpenAPIConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/config/OpenAPIConfig.java
@@ -2,6 +2,8 @@ package com.devsuperior.dsmovie.config;
 
 import io.swagger.v3.oas.annotations.OpenAPI31;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
@@ -10,19 +12,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer")
 public class OpenAPIConfig {
 
     @Bean
     public OpenAPI dsmovieAPI(){
         return new OpenAPI()
-                .info(new Info()
-                    .title("DSMovie API")
-                    .description("DSMovie Reference Project")
-                    .version("v0.0.1")
-                    .license(new License()
-                        .name("Apache 2.0")
-                        .url("https://github.com/7E0n4Rd0/dsmovie-ref")
-                    )
-                );
+            .info(new Info()
+                .title("DSMovie API")
+                .description("DSMovie Reference Project")
+                .version("v0.0.1")
+                .license(new License()
+                    .name("Apache 2.0")
+                    .url("https://github.com/7E0n4Rd0/dsmovie-ref")
+                )
+            );
     }
 }
